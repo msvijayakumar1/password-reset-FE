0ad1dc67b25448f73d35e8c023240eb1edf6ba63
Vijayakumar <msvijayakumar1>
2023-03-29 04:42:01 -0700

    Commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/',
+  baseURL: 'https://password-reset-e6q8.onrender.com/',
 })
 
 export const signIn = (formData) => API.post(`/users/signin`, formData)
